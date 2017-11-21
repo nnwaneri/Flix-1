118dbeeb31f6975ae43634a72b4c81678b757322
Harold Lomotey <haroldlomotey@gmail.com>
2017-11-20 19:58:43 -0500

    Fourth Commit

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Harold  on 11/20/17.
+//  Copyright © 2017 Harold . All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+       
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+
+}
---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,102 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Harold  on 11/20/17.
+//  Copyright © 2017 Harold . All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var movies: [[String: Any]] = []
+    var refreshControl: UIRefreshControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.startAnimating()
+        
+        tableView.dataSource = self
+        self.tableView.rowHeight = 170.0
+        
+        
+        fetchMovieData()
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+     
+        refreshControl.attributedTitle = NSAttributedString(string: "Fetching Movies")
+   
+        tableView.insertSubview(refreshControl, at: 0)
+        
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+        fetchMovieData()
+    }
+    
+    func fetchMovieData() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=cddafef891a214cf8df28c4334102412")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This would run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String:Any]]
+                
+                self.movies = movies
+                self.tableView.reloadData()
+                self.activityIndicator.stopAnimating()
+                self.refreshControl.endRefreshing()
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        
+        let posterPathString = movie["poster_path"] as! String
+        let baseUrlString = "https://image.tmdb.org/t/p/w500"
+        let posterUrl = URL(string: baseUrlString + posterPathString)!
+        cell.posterImageView.af_setImage(withURL: posterUrl)
+        
+        cell.posterImageView.layer.cornerRadius = 10.0
+        cell.posterImageView.clipsToBounds = true
+        
+        return cell
+    }
+
+    
+    
+
+
+
+}
